8377c9ae6a1fcb282911f63c3f941cbd5740227a
Dxtag <58232295+Dxtag@users.noreply.github.com>
2020-05-05 23:35:06 +0200

    Update app.py

--- FLASK/app.py
+++ FLASK/app.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template, url_for, request, flash, redirect
+from flask import Flask, render_template, url_for, request, flash, redirect, session
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "]xf7x9fxabxcfx87{xadjxd3exea4RYA"
 
 
 @app.route("/")
-@app.route("/home")
-def home():
+def index():
     return render_template('index.html')
 
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    pass
 
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    pass
+
+@app.route("/rules")
+def rules():
+    pass
+
+        
 if __name__ == '__main__':
     app.run(debug=True)
