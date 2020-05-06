ac2bfe63dfc84051a7dc5ad9ad5f426b9d6d5bc8
Dxtag <58232295+Dxtag@users.noreply.github.com>
2020-05-06 21:45:32 +0200

    Create forms.py

---
+++ FLASK/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
+class Register(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
+    token = StringField("Token", validators=[DataRequired())
+    submit = SubmitField("Sign Up")
+
+
+class Login(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
