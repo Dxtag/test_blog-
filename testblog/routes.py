1df84eb3c3f752085c0b89994663075369f8c701
Dxtag <58232295+Dxtag@users.noreply.github.com>
2020-05-08 13:48:27 +0200

    Add files via upload

---
+++ testblog/routes.py
@@ -0,0 +1,22 @@
+from flask import render_template, url_for, flash, redirect
+from testblog.forms import Register, Login
+from testblog import app
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    login = Login()
+    return render_template('login.html', title = "login",login = login)
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    register = Register()
+    return render_template('register.html', title = "register",register = register)
+
+@app.route("/rules")
+def rules():
+    return render_template('rules.html', title = "rules")
