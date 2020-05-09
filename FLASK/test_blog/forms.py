4ad4c5e4f4541d9257a43a1f11c8d70cee5d5afc
Dxtag <58232295+Dxtag@users.noreply.github.com>
2020-05-09 13:13:49 +0200

    Update forms.py

--- FLASK/test_blog/forms.py
+++ FLASK/test_blog/forms.py
@@ -5,16 +5,15 @@ from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class Register(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
-    token = StringField("Token", validators=[DataRequired()])
-    submit = SubmitField("Sign Up")
 
 
 class Login(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
+    remember_me = BooleanField("Remember me')
                                              
 # Trzeba dorzucić Flask-Mail i itsdangerous żeby zrobić tokena ale to raczej w osobnym pliku 
