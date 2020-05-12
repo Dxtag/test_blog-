4a54da97e5764c004b0c39eeb4d229a2a4f05ab1
Dxtag <dxtagp+github@gmail.com>
2020-05-12 12:31:05 +0200

    usuwanie postow

--- FLASK/test_blog/routes.py
+++ FLASK/test_blog/routes.py
@@ -4,10 +4,12 @@ from test_blog import app , db, bcrypt, login_manager
 from test_blog.data_forms import Profile, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
+@app.route("/index")
+@app.route("/home")
 @app.route("/")
 def index():
-    
-    return render_template("index.html")
+    posts =  Post.query.all()
+    return render_template("index.html", posts=posts)
 
 @app.route("/login", methods = ["POST", "GET"])
 def login():
@@ -18,7 +20,7 @@ def login():
         user = Profile.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember_me.data)
-            flash("Sucess!","success")
+            flash("Sucess!","info")
             return redirect(url_for("account"))
         else:
             flash("Something wrong","danger")
@@ -34,7 +36,7 @@ def register():
         profile = Profile(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(profile)
         db.session.commit()
-        flash("Account created!", "success")
+        flash("Account created!", "info")
         return redirect(url_for("login"))
     return render_template("register.html", title = "register",form = form)
 
@@ -42,14 +44,27 @@ def register():
 def rules():
     return render_template("rules.html", title = "rules")
 
-@app.route("/account")
+@app.route("/account", methods=["POST", "GET"])
 @login_required
 def account():
-    add_post = Post_form()
-    return render_template("account.html", title=current_user.username, add_post=add_post)
+    form = Post_form()
+    if form.validate_on_submit():
+        post = Post(topic=form.topic.data, content=form.content.data, profile_id=current_user.id)
+        db.session.add(post)
+        db.session.commit()
+        flash("Post added!", "info")
+    posts = Post.query.filter_by(profile_id=current_user.id).all()
+    return render_template("account.html", title=current_user.username, form=form, posts=posts)
 
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
+@app.route("/delete_post/<post_id>")
+@login_required
+def delete_post(post_id):
+    Post.query.filter(Post.id == post_id).delete()
+    db.session.commit()
+    return redirect(url_for("account"))
